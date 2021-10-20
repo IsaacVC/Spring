360712542b25c70a1401de61b133ddee4fd8c678
IsaacVC <isaacvilchezc20@gmail.com>
2021-10-20 18:45:25 -0500

    IniciandoRepositorio

---
+++ src/main/java/pe/com/iv/domain/Persona.java
@@ -0,0 +1,11 @@
+package pe.com.iv.domain;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
