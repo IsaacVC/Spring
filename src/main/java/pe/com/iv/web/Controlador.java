103ac2925fb34076b7b89db81e5bca14cf8eed22
IsaacVC <isaacvilchezc20@gmail.com>
2021-10-20 21:26:00 -0500

    Agregando dependencias de MySql

---
+++ src/main/java/pe/com/iv/web/Controlador.java
@@ -0,0 +1,53 @@
+package pe.com.iv.web;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import lombok.var;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pe.com.iv.domain.Persona;
+
+@Controller
+@Slf4j
+public class Controlador {
+
+
+    @GetMapping("/")
+    public String Inicio(Model model) {
+
+//        Persona persona = new Persona();
+//        persona.setNombre("Isaac");
+//        persona.setApellido("Vilchez C");
+//        persona.setEmail("i@vilchez.com");
+//        persona.setTelefono("921883655");
+//
+//        Persona persona2 = new Persona();
+//        persona2.setNombre("Karla");
+//        persona2.setApellido("Gomez");
+//        persona2.setEmail("Karl@vilchez.com");
+//        persona2.setTelefono("998556841");
+//
+//        Persona persona3 = new Persona();
+//        persona3.setNombre("Juan");
+//        persona3.setApellido("Perez");
+//        persona3.setEmail("JPerez@vilchez.com");
+//        persona3.setTelefono("985563589");
+//
+//        //List<Persona> personas = new ArrayLit();
+//        //var personas = Arrays.asList(persona, persona2);
+//        List<Persona> personas = new ArrayList();
+//        personas.add(persona);
+//        personas.add(persona2);
+//        personas.add(persona3);
+        //personas.add(persona2);
+
+        log.info("Ejecutando el controlador Spring MVC");
+        //model.addAttribute("personas", personas);
+        return "index";
+    }
+
+}
