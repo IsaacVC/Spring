3be5475a63ff0d31e38e2461d2ebb90b1d0c35ce
IsaacVC <isaacvilchezc20@gmail.com>
2021-10-27 05:24:05 -0500

    Validacion de datos

--- src/main/java/pe/com/iv/domain/Persona.java
+++ src/main/java/pe/com/iv/domain/Persona.java
@@ -2,7 +2,11 @@ package pe.com.iv.domain;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
+
+
 
 @Data
 @Entity
@@ -14,9 +18,19 @@ public class Persona implements Serializable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer id; 
+    
+    @NotEmpty
     private String nombre;
+    
+    @NotEmpty
     private String apellido;
+    
+    @NotEmpty
+    @Email
     private String email;
+    
+    @NotEmpty    
     private String telefono;
+    
 }
--- src/main/java/pe/com/iv/web/Controlador.java
+++ src/main/java/pe/com/iv/web/Controlador.java
@@ -1,11 +1,13 @@
 package pe.com.iv.web;
 
 import java.util.List;
+import javax.validation.Valid;
 import lombok.experimental.var;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import pe.com.iv.domain.Persona;
@@ -33,7 +35,10 @@ public class Controlador {
     }
     
     @PostMapping("/guardar")
-    public String guardar(Persona persona){
+    public String guardar(@Valid Persona persona, Errors errores){
+        if(errores.hasErrors()){
+            return "modificar";
+        }        
         personaService.guardar(persona);
         return "redirect:/";
     }
